5a4e22f4d6f121d272dcde79478ce37f06947481
smohan <smohan@overstock.com>
2020-01-24 23:32:30 -0700

    New Commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import MyFooter from "./Footer";
+import NavBar from "./Navbar"
+import Header from "./Header"
+
+function MyInfo() {
+
+return(
+    <div>
+        <NavBar/>
+        <Header/>
+        <MyFooter />
+    </div>
+);
+}
+
+export default MyInfo;
